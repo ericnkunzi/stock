d4ba9126598947a53176d10718340f54b73ab255
ericnkunzi <168414577+ericnkunzi@users.noreply.github.com>
2025-05-19 11:18:40 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBe_0pC4wg2L7RDxzM-ncdTDQUEm9r28UY",
   authDomain: "stockmanagementapp-2d6e7.firebaseapp.com",
   projectId: "stockmanagementapp-2d6e7",
-  storageBucket: "stockmanagementapp-2d6e7.firebasestorage.app",
+  storageBucket: "stockmanagementapp-2d6e7.appspot.com", // ✅ Fixed
   messagingSenderId: "411480450139",
   appId: "1:411480450139:web:7d34243e1eba5113c04b7e",
   measurementId: "G-LS4RF6YXVR"
